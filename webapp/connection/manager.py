052e28960ec89266952f3cb4df47130d87c1fe93
lilu <1606537102@qq.com>
2024-07-05 14:44:28 +0800

    notes

--- webapp/connection/manager.py
+++ webapp/connection/manager.py
@@ -3,6 +3,11 @@ from typing import List
 
 class ConnectionManager:
     def __init__(self):
+        """
+        The default behavior of the `__hash__` method for an instance of a class is as follows:
+            1. It calls the `id()` function to obtain the memory address of the object. This address remains constant during the object's lifetime. Once an object is created, its `id()` is fixed until the object is destroyed, and changes to the object's content do not affect its memory address. In other words, for a given object, the `id()` function always returns the same value while the object exists.
+            2. This memory address is used as the hash value.
+        """
         self.active_connections: List[WebSocket] = []
         self.usernames: dict[WebSocket, str] = {}
 
